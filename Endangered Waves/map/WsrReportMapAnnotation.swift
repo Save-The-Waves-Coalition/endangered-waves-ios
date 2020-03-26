174ed7c068302f0c2d6f1de646317006f3f1127d
Erik <erik.parr@valtech.com>
2020-03-26 15:45:35 -0700

    added WSR placemarkers.  Placemarkers are working, but still need to add icons, Wsr report etc.

---
+++ Endangered Waves/map/WsrReportMapAnnotation.swift
@@ -0,0 +1,28 @@
+//
+//  ReportMapAnnotation.swift
+//  Endangered Waves
+//
+//  Created by Matthew Morey on 11/12/17.
+//  Copyright © 2017 Save The Waves. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class ReportMapAnnotation: NSObject, MKAnnotation {
+
+    var title: String? {
+        return report.name
+    }
+    var subtitle: String? {
+        return report.description
+    }
+    let coordinate: CLLocationCoordinate2D
+    let report: Report
+
+    init(coordinate: CLLocationCoordinate2D, report: Report) {
+        self.coordinate = coordinate
+        self.report = report
+        super.init()
+    }
+}
